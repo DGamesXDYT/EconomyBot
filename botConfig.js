641e08b4992331dc2781b984dfc38efafb72e580
DGames_xD <96751575+DGamesXDYT@users.noreply.github.com>
2022-05-22 13:49:37 +0200

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     token: "OTc3NjUyMDExNzE1Njc0MjAy.Gy71Z3.cgkVrz-hYQ5Z5-l-EkVZcLBkdqBa4SXUx_YLi8",
     prefix: "e",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "727945605179113613, 711640465400791093"
 ],
     debug: true,
     countChannel: "countChannelID"
