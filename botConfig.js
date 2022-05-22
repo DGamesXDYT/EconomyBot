73b39ebdd048bbeb70282249b46565d5d3b37b19
DGames_xD <96751575+DGamesXDYT@users.noreply.github.com>
2022-05-22 13:42:20 +0200

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "OTc3NjUyMDExNzE1Njc0MjAy.Gy71Z3.cgkVrz-hYQ5Z5-l-EkVZcLBkdqBa4SXUx_YLi8",
+    prefix: "e",
     admins: [
         "PEOPLE WHO CAN USE ADD MONEY (IDS)"
 ],
